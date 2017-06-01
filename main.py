9a5643fec837482a07fc53ee0d4b694e819745a1
qiray <zotovyaa@mail.ru>
2017-06-01 22:53:16 +0300

    multiuser support + log added

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import time
 import sqlite3
 import telegram_token #файл с токеном
 
-errorfile = "Error.log"
 dbfile = 'data.db'
 
 class Userdata:
@@ -20,6 +19,23 @@ class Userdata:
 		self.request_step = request_step
 		self.request_string = request_string
 		self.request_data = request_data
+
+class Log:
+	logfile = "log.txt"
+	def __init__(self, filename):
+		self.logfile = filename 
+
+	def debug(self, string):
+		print string
+		with open(self.logfile, "a") as myfile:
+			myfile.write(string.encode('utf8') + "\r\n")
+
+	def error(self, string):
+		print string
+		with open(self.logfile, "a") as myfile:
+			myfile.write("\r\n\r\n" + time.strftime("%c")+"\r\n<<ERROR>>\r\n"+ string.encode('utf8') + "\r\n<<ERROR>>")
+
+log = Log("log.txt")
 		
 users = {}
 
@@ -33,38 +49,31 @@ requestBasicText = 'https://fantlab.ru/bygenre?'
 
 logicalorrequest = '&logicalor=on' # ИЛИ вместо И
 
-additionalRequestText = ""
-requestStep = 0
-requestData = None
-
-def initData():
+def initData(chat_id):
+	users[chat_id] = Userdata(0, "", None) 
 	random.seed()
-	global requestStep
-	global additionalRequestText
-	global requestData
-	requestData = None
-	requestStep = 0
-	additionalRequestText = ""
 
 def startFunc(message):
 	messageChatId = message.chat.id
+	users[messageChatId] = Userdata(0, "", None) #init data
 	keyboard = types.InlineKeyboardMarkup()
 	startButton = types.InlineKeyboardButton(text = texts.showRecomendsText, callback_data = "/book")
 	keyboard.add(startButton)
 	bot.send_message(messageChatId, texts.startText, reply_markup = keyboard)
 
 def recomendationFunc(message):
-	global requestStep
 	messageChatId = message.chat.id
+	if not messageChatId in users:
+		users[messageChatId] = Userdata(0, "", None) 
 	keyboard = types.InlineKeyboardMarkup()
-	for i in texts.requestStepsArray[requestStep]:
+	for i in texts.requestStepsArray[users[messageChatId].request_step]:
 		keyboard.add(types.InlineKeyboardButton(text = i[0], callback_data = i[1]))
-	bot.send_message(messageChatId, texts.requestNamesArray[requestStep], reply_markup = keyboard)
-	requestStep += 1
+	bot.send_message(messageChatId, texts.requestNamesArray[users[messageChatId].request_step], reply_markup = keyboard)
+	users[messageChatId].request_step += 1
 	
 def getData(request):
 	request = re.sub("\?&", "?", request)
-	print request
+	log.debug(request)
 	soup = BeautifulSoup(urllib2.urlopen(request).read())
 	table = soup.find('table')
 	rows = table.findAll('tr')
@@ -74,22 +83,22 @@ def getData(request):
 	return rows
 
 def showResult(message):
-	global requestData
-	global additionalRequestText
-	if requestData == None:
-		if additionalRequestText == "":
-			additionalRequestText = texts.fixRequestString
-		requestData = getData(requestBasicText + additionalRequestText + logicalorrequest)
-	index = random.randint(3, len(requestData) - 1)
-	cols = requestData[index].findAll('td')
+	messageChatId = message.chat.id
+	if not messageChatId in users:
+		users[messageChatId] = Userdata(0, "", None) 
+	if users[messageChatId].request_data == None:
+		if users[messageChatId].request_string == "":
+			users[messageChatId].request_string = texts.fixRequestString
+		users[messageChatId].request_data = getData(requestBasicText + users[messageChatId].request_string + logicalorrequest)
+	index = random.randint(3, len(users[messageChatId].request_data) - 1)
+	cols = users[messageChatId].request_data[index].findAll('td')
 	result = cols[0].getText(separator = u' ')
 	result = re.sub(r"^\d+\. ", "", result)
 	result = re.sub(' +',' ', result) 
 	href = cols[0].findAll('a')[0]['href']
-	print href
 	href = "https://fantlab.ru" + re.findall("/work.*|$", href)[0]
 	result = result + "\n" + href
-	print result
+	log.debug(result)
 	keyboard = types.InlineKeyboardMarkup()
 	button1 = types.InlineKeyboardButton(text = texts.iwantmoreText, callback_data = "wantanotherbook")
 	button2 = types.InlineKeyboardButton(text = texts.startagainText, callback_data = "/book")
@@ -99,54 +108,56 @@ def showResult(message):
 
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
-	messageChatId = message.chat.id
-	print message.from_user.id, messageChatId
-	messageText = message.text
-	if messageText == '/start':
-		startFunc(message)
-	elif messageText == '/help':
-		bot.send_message(messageChatId, texts.helpText)
-	elif messageText == '/book':
-		initData()
-		recomendationFunc(message)
-	else:
-		startFunc(message)
+	try: 
+		messageChatId = message.chat.id
+		messageText = message.text
+		log.debug(messageText)
+		if messageText == '/start':
+			startFunc(message)
+		elif messageText == '/help':
+			bot.send_message(messageChatId, texts.helpText)
+		elif messageText == '/book':
+			initData(messageChatId)
+			recomendationFunc(message)
+		else:
+			startFunc(message)
+	except Exception as e:
+		log.debug('\nFailed: ' + call.data + "\r\n" + str(e) + "\r\n")
+		log.error(traceback.format_exc())
+		bot.send_message(call.message.chat.id, texts.botErrorText)
 		
 @bot.callback_query_handler(func = lambda call: True)
 def callback_inline(call):
 	try:
 		if call.message:
+			log.debug(call.message.text + " " + call.data)
+			messageChatId = call.message.chat.id
 			if call.data == '/book':
-				initData()
+				initData(messageChatId)
 				recomendationFunc(call.message)
 			elif call.data == 'wantanotherbook':
 				showResult(call.message)
-			else: 
-				if requestStep >= len(texts.requestStepsArray):
+			else:
+				if not messageChatId in users:
+					users[messageChatId] = Userdata(0, "", None) 
+				if users[messageChatId].request_step >= len(texts.requestStepsArray):
 					bot.send_message(call.message.chat.id, texts.pleaseWaitText)
 					showResult(call.message)
 					return
-				global additionalRequestText
 				if call.data != 'NONE': #собираем строку запроса
-					additionalRequestText += call.data
-				print additionalRequestText
+					users[messageChatId].request_string += call.data
+				log.debug(str(messageChatId) + " " + users[messageChatId].request_string)
 				recomendationFunc(call.message)
 	except Exception as e:
-		print '\nFailed: ' + call.data
-		print str(e) + "\n"
-		traceback_error_string=traceback.format_exc()
-		with open(errorfile, "a") as myfile:
-			myfile.write("\r\n\r\n" + time.strftime("%c")+"\r\n<<ERROR>>\r\n"+ traceback_error_string + "\r\n<<ERROR>>")
-		traceback.print_exc(file=sys.stdout)
+		log.debug('\nFailed: ' + call.data + "\r\n" + str(e) + "\r\n")
+		log.error(traceback.format_exc())
 		bot.send_message(call.message.chat.id, texts.botErrorText)
 
 def telegram_polling(): #TODO: test!
 	try:
 		bot.polling(none_stop=True, timeout = 60) #constantly get messages from Telegram
 	except:
-		traceback_error_string=traceback.format_exc()
-		with open(errorfile, "a") as myfile:
-			myfile.write("\r\n\r\n" + time.strftime("%c")+"\r\n<<ERROR polling>>\r\n"+ traceback_error_string + "\r\n<<ERROR polling>>")
+		log.error(traceback.format_exc())
 		bot.stop_polling()
 		time.sleep(10)
 		telegram_polling()
